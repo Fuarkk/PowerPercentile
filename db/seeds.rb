34b4434887a95ed1f463e640991d83b108617f40
= <=>
2020-05-28 20:50:11 +0100

    DATABASE database migrated and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach('OPLData/DATAFINAL.csv', headers: true) do |row|
+
+  tested = row[13] == "Yes" ? true : false;
+
+  lifter = Lifter.find_or_create_by name:  row[0],
+                           sex:            row[1],
+                           event:          row[2],
+                           equipment:      row[3],
+                           age:            row[4],
+                           division:       row[5],
+                           bodyweightKg:   row[6],
+                           weightClassKg:  row[7],
+                           squat:          row[8],
+                           bench:          row[9],
+                           deadlift:       row[10],
+                           total:          row[11],
+                           place:          row[12],
+                           tested:         tested,
+                           country:        row[14],
+                           federation:     row[15],
+                           meetDate:       row[16]
+
+    puts "added lifter: #{lifter.id} - #{lifter.name}"
+end
