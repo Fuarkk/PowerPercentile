ffb09615559f64f9a161dafb27e385aeebcb7720
= <=>
2020-05-30 19:21:08 +0100

    GRAPHING_DATA some data graphing, modifications to graph scaling, still many issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def submit_data
+    puts params
+
+    @data = Lifter.where.has{|l| (l.weightClassKg == params[:weightClass]) & (l.total != nil)}
+    puts @data.count
+
+    squat    = (@data.where.has{|l| l.squat != nil}).order(squat: :asc).pluck(:squat);
+    bench    = (@data.where.has{|l| l.bench != nil}).order(bench: :asc).pluck(:bench);
+    deadlift = (@data.where.has{|l| l.deadlift != nil}).order(deadlift: :asc).pluck(:deadlift);
+    total    = (@data.where.has{|l| (l.bench != nil) && (l.squat != nil) && (l.deadlift != nil)}).order(total: :asc).pluck(:total);
+
+    @data = [squat, bench, deadlift, total];
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home', to: 'home#index', as: :home
+
+
+    get 'submit_data', controller: :home
+
+
+
 end
